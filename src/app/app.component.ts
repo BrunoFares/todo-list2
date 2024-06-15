fcd7d2983b4375755dd1ec02127965c77c56ddb7
BrunoFares <faresbruno04@gmail.com>
2024-06-16 02:16:41 +0300

    Fixed the general layout.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from './header/header.component';
+import { TodoListsComponent } from './todo-lists/todo-lists.component';
+import { TodoListComponent } from './todo-list/todo-list.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeaderComponent],
+  imports: [HeaderComponent, TodoListsComponent, TodoListComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
